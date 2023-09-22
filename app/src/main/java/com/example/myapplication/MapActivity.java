9c231b058e5195248615cbb63d9585abcd4418fc
adrian001121 <adrianyikiat@student.unimelb.edu.au>
2023-09-22 14:20:38 +1000

    Adding the features and geojson

--- app/src/main/java/com/example/myapplication/MapActivity.java
+++ app/src/main/java/com/example/myapplication/MapActivity.java
@@ -8,8 +8,19 @@ import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.Style;
+import com.mapbox.mapboxsdk.maps.*;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.geojson.Point;
+import com.mapbox.geojson.Feature;
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
+import com.mapbox.mapboxsdk.style.layers.CircleLayer;
+import com.mapbox.mapboxsdk.style.layers.PropertyFactory;
 
+
+import android.graphics.Color;
 import android.os.Bundle;
 
 public class MapActivity extends AppCompatActivity{
@@ -35,11 +46,35 @@ public class MapActivity extends AppCompatActivity{
                     @Override
                     public void onStyleLoaded(@NonNull Style style) {
 
+                        // Set initial map viewport and zoom
+                        CameraPosition initialPosition = new CameraPosition.Builder()
+                                .target(new LatLng(-37.80995133438894, 144.96871464972733))  // Set the latitude and longitude
+                                .zoom(10)  // Set zoom level
+                                .build();
+
+                        mapboxMap.moveCamera(CameraUpdateFactory.newCameraPosition(initialPosition));
+
+                        // Add a data source
+                        GeoJsonSource geoJsonSource = new GeoJsonSource("source-id",
+                                Feature.fromGeometry(Point.fromLngLat(144.96871464972733, -37.80995133438894)));
+                        style.addSource(geoJsonSource);
+
+                        // Add a layer to use the source
+                        CircleLayer circleLayer = new CircleLayer("layer-id", "source-id");
+                        circleLayer.withProperties(
+                                PropertyFactory.circleRadius(10f),
+                                PropertyFactory.circleColor(Color.argb(127, 255, 0, 0))
+                        );
+                        style.addLayer(circleLayer);
+
                         // Map is set up and the style has loaded. Now you can add data or make other map adjustments
 
 
                     }
                 });
+
+                // Enable zoom controls (+ and - buttons)
+                mapboxMap.getUiSettings().setZoomGesturesEnabled(true);
 
             }
         });
