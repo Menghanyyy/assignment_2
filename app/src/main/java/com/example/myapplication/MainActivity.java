436cc3965a418c995dfcf8ade8bce322eaa8a0de
adrian001121 <adrianyikiat@student.unimelb.edu.au>
2023-09-24 17:54:09 +1000

    Adding Object Class

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.myapplication;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.myapplication.component.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    protected void onStart() {
+        super.onStart();
+        User abc = new GeneralUser("001", "abc", "cc@gmail.com", "0011");
+
+        Log.i("test user", abc.toString());
+
+        Visit testV = new Visit(abc.getUserId(), null, null);
+        Log.i("test visit", testV.toString());
+
     }
 }
---
+++ app/src/main/java/com/example/myapplication/component/OrganisationUser.java
@@ -0,0 +1,52 @@
+package com.example.myapplication.component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrganisationUser extends User{
+
+    private List<Event> hostingEvent;
+    private String subscribeLink;
+    private List<User> subscriber;
+
+
+    public OrganisationUser(String UserId, String UserName, String UserEmail, String UserPin) {
+        super(UserId, UserName, UserEmail, UserPin);
+
+        this.hostingEvent = new ArrayList<Event>();
+
+        this.subscribeLink = "abc.com";
+
+        this.subscriber = new ArrayList<User>();
+
+    }
+
+    public List<Event> getHostingEvent() {
+        return this.hostingEvent;
+    }
+
+    public boolean addHostingEvent (Event event) {
+       return this.hostingEvent.add(event);
+    }
+
+    public String getSubscribeLink() {
+        return this.subscribeLink;
+    }
+
+    public List<User> getSubscriber() {
+        return this.subscriber;
+    }
+
+    public boolean addSubscriber(User user) {
+        return this.subscriber.add(user);
+    }
+
+    @Override
+    public String toString() {
+        return "OrganisationUser{" +
+                "hostingEvent=" + hostingEvent +
+                ", subscribeLink='" + subscribeLink + '\'' +
+                ", subscriber=" + subscriber +
+                '}';
+    }
+}
