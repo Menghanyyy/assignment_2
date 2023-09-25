4cba4a47fdf469547624f3b68dd4b4305edfc9c7
Latti31 <111042692+Latti31@users.noreply.github.com>
2023-09-25 21:36:16 +1000

    Added new class for holding location

--- app/src/main/java/com/example/myapplication/location/GPSLocation.java
+++ app/src/main/java/com/example/myapplication/location/GPSLocation.java
@@ -1,2 +1,12 @@
-package com.example.myapplication.location;public class GPSLocation {
+package com.example.myapplication.location;
+
+public class GPSLocation {
+    private final double longitude;
+    private final double latitude;
+    public GPSLocation(double latitude, double longitude){
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+    public double getLongitude(){ return longitude; }
+    public double getLatitude(){ return latitude; }
 }
