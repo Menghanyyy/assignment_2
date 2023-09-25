ef01c0f14eb46a087779a271bc98dbb9da561006
Latti31 <111042692+Latti31@users.noreply.github.com>
2023-09-25 21:35:34 +1000

    Added new interface methods

--- app/src/main/java/com/example/myapplication/database/DatabaseInterface.java
+++ app/src/main/java/com/example/myapplication/database/DatabaseInterface.java
@@ -1,64 +1,64 @@
 package com.example.myapplication.database;
 
 import com.example.myapplication.component.*;
+import com.example.myapplication.location.GPSLocation;
 
 import java.util.ArrayList;
 
 public interface DatabaseInterface {
 
-    // Event
+    ///////////////// Event Methods /////////////////
+
     public boolean addEvent(Event event);
 
-    public Event getEventByID(int organisationID, int eventID);
+    public Event getEventByID(int eventID);
 
     public ArrayList<Event> getAllEvents();
 
-
-    // Activity
-    public boolean addActivity(Activity activity);
-
-    public Activity getActivityByID(int organisationID, int eventID, int activityID);
-
-    public ArrayList<Activity> getAllActivities();
-
-
-
-    // User (General)
-    public boolean addUser(GeneralUser user);
-
-    public GeneralUser getUserByID(int userID);
-
-    public ArrayList<GeneralUser> getAllUsers();
-
-
-    // Creator (Organisation User)
-    public boolean addCreator(OrganisationUser creator);
-
-    public OrganisationUser getCreatorByID(int creatorID);
-
-    public ArrayList<OrganisationUser> getAllCreators();
-
-
-    // Visit
-    public boolean addVisit(Visit visit);
-
-    public Visit getVisitByID(int userID, int activityID, int eventID, int organisationID);
-
-    // Gives the total visits for a specific activity
-    public int visitCountAtActivity(int organisationID, int eventID, int activityID);
-
-    // Gives the total visits at an event for a specific user
-    public int visitCountForUserAtEvent(int userID, int organisationID, int eventID);
-
-
-    // Join (Users signing up for events)
-    public boolean joinEvent(GeneralUser user, Event event);
-
-    public boolean quitEvent(GeneralUser user, Event event);
+    // Users signing up to attend an event
+    public boolean joinEvent(User user, Event event);
 
     // Returns all users currently signed up for an event
     public ArrayList<User> getUsersAtEvent(Event event);
 
     // Returns all events an individual has signed up for
     public ArrayList<Event> getJoinedEvents(User user);
+
+
+
+    ///////////////// Activity Methods /////////////////
+
+    public boolean addActivity(Activity activity);
+
+    public Activity getActivityByID(int eventID, int activityID);
+
+    public ArrayList<Activity> getAllActivities();
+
+    // Visit (Visit class includes user/event details)
+    public boolean addVisit(Visit visit);
+
+    public Visit getVisitByID(int userID, int activityID, int eventID);
+
+    // Gives the total visits for a specific activity
+    public int visitCountAtActivity(int eventID, int activityID);
+
+    // Gives the total visits at an event for a specific user
+    public int visitCountForUserAtEvent(int userID, int eventID);
+
+
+
+    ///////////////// User Methods /////////////////
+
+    public boolean addUser(User user);
+
+    public GeneralUser getUserByID(int userID);
+
+    public ArrayList<GeneralUser> getAllUsers();
+
+
+
+    ///////////////// Location Methods /////////////////
+
+    // Gives all the activities (in a given event) that overlap a point on the map
+    public ArrayList<Activity> eventsAtLocation(GPSLocation location, Event event);
 }
---
+++ app/src/main/java/com/example/myapplication/location/GPSLocation.java
@@ -0,0 +1,2 @@
+package com.example.myapplication.location;public class GPSLocation {
+}
