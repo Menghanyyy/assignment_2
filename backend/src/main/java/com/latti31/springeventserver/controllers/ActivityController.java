104d9b590c59463c1618b81a6939951799c2cb74
Latti31 <111042692+Latti31@users.noreply.github.com>
2023-09-26 12:56:20 +1000

    Added get all activities for event functionality

--- backend/src/main/java/com/latti31/springeventserver/controllers/ActivityController.java
+++ backend/src/main/java/com/latti31/springeventserver/controllers/ActivityController.java
@@ -2,6 +2,7 @@ package com.latti31.springeventserver.controllers;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
@@ -139,12 +140,67 @@ public class ActivityController {
         }
     }
 
+    // Method to retrieve all activities
+    @GetMapping("/getAll/{event_id}")
+    public String getAll(@PathVariable int event_id) {
+        String query = "SELECT " +
+                "activityID, " +
+                "ST_AsText(centreLocation) AS centreLocation, " +
+                "ST_AsText(polygonLocation) AS polygonLocation, " +
+                "ST_AsText(bbox) AS bbox, " +
+                "description, " +
+                "startTime, " +
+                "endTime, " +
+                "name, " +
+                "eventID, " +
+                "locationName," +
+                "backgroundPicture, " +
+                "creatorID " +
+                "FROM Activity WHERE eventID = ?";
 
+        try {
+            List<Map<String, Object>> activities = jdbcTemplate.queryForList(query, event_id);
+            List<ObjectNode> activityList = new ArrayList<>(); // List to store activity JSON objects
 
-    // Additional method to retrieve all events
-//    @GetMapping("/all")
-//    public List<String> getAllEvents() {
-//        String query = "SELECT event_data FROM Event";
-//        return jdbcTemplate.queryForList(query, String.class);
-//    }
+            for (int i = 0; i < activities.size(); i++) {
+                Map<String, Object> activity = activities.get(i);
+
+                // Retrieve the BLOB data as a byte array
+                byte[] backgroundPictureData = (byte[]) activity.get("backgroundPicture");
+
+                // Convert the byte array to Base64 encoding
+                String backgroundPictureBase64 = Base64.getEncoder().encodeToString(backgroundPictureData);
+
+                // Create a JSON object for the current activity
+                ObjectMapper objectMapper = new ObjectMapper();
+                ObjectNode jsonObject = objectMapper.createObjectNode();
+                jsonObject.put("activityID", (Integer) activity.get("activityID"));
+                jsonObject.put("centreLocation", (String) activity.get("centreLocation"));
+                jsonObject.put("polygonLocation", (String) activity.get("polygonLocation"));
+                jsonObject.put("bbox", (String) activity.get("bbox"));
+                jsonObject.put("description", (String) activity.get("description"));
+                jsonObject.put("startTime", activity.get("startTime").toString());
+                jsonObject.put("endTime", activity.get("endTime").toString());
+                jsonObject.put("name", (String) activity.get("name"));
+                jsonObject.put("eventID", (Integer) activity.get("eventID"));
+                jsonObject.put("locationName", (String) activity.get("locationName"));
+                jsonObject.put("backgroundPicture", backgroundPictureBase64);
+                jsonObject.put("creatorID", (Integer) activity.get("creatorID"));
+
+                // Add the current activity JSON object to the list
+                activityList.add(jsonObject);
+            }
+
+            // Convert the list of activity JSON objects to a JSON array
+            ObjectMapper objectMapper = new ObjectMapper();
+            ArrayNode jsonArray = objectMapper.valueToTree(activityList);
+
+            // Convert JSON array to a JSON string
+            return jsonArray.toString();
+        } catch (Exception e) {
+            // Handle exceptions, e.g., if there are no activities for the specified event
+            return "Error retrieving activities: " + e.getMessage();
+        }
+    }
+
 }
