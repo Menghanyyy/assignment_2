714e2cb490ace816ab83c5f450b8d4257a91685f
Latti31 <111042692+Latti31@users.noreply.github.com>
2023-09-26 14:31:44 +1000

    removed eventID as part of the comp key for activity

--- backend/src/main/java/com/latti31/springeventserver/objects/DatabaseChecker.java
+++ backend/src/main/java/com/latti31/springeventserver/objects/DatabaseChecker.java
@@ -29,6 +29,31 @@ public class DatabaseChecker {
         }
     }
 
+    public boolean keyNotInDB(
+            String tableName,
+            String primaryKeyColumnName1,
+            int primaryKeyValue1,
+            String primaryKeyColumnName2,
+            int primaryKeyValue2
+        ) {
+        String surroundedTableName = "`" + tableName + "`";
+        String query = "SELECT COUNT(*) FROM " + surroundedTableName +
+                " WHERE " + primaryKeyColumnName1 + " = ?" +
+                "AND " + primaryKeyColumnName2 + " = ?";
 
+        try {
+            // Execute the query and retrieve the result as a list of maps
+            List<Map<String, Object>> result = jdbcTemplate.queryForList(
+                    query,
+                    primaryKeyValue1,
+                    primaryKeyValue2
+            );
+
+            // Check if the result is empty or the count is not greater than 0
+            return result.isEmpty() || ((Number) result.get(0).get("COUNT(*)")).intValue() <= 0;
+        } catch (Exception e) {
+            return true;
+        }
+    }
 
 }
