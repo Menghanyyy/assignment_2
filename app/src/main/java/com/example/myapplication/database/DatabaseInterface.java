79ad8e249dfd11c8cf102e6e787f5e5b48e0ac99
Latti31 <111042692+Latti31@users.noreply.github.com>
2023-09-27 23:53:25 +1000

    Added callback to args for all methods

--- app/src/main/java/com/example/myapplication/database/DatabaseInterface.java
+++ app/src/main/java/com/example/myapplication/database/DatabaseInterface.java
@@ -9,62 +9,62 @@ public interface DatabaseInterface {
 
     ///////////////// Event Methods /////////////////
 
-    public boolean addEvent(Event event);
+    public boolean addEvent(Event event, final DatabaseCallback<Integer> callback);
 
-    public Event getEventByID(int eventID);
+    public Event getEventByID(int eventID, final DatabaseCallback<Integer> callback);
 
-    public ArrayList<Event> getAllEvents();
+    public ArrayList<Event> getAllEvents(final DatabaseCallback<Integer> callback);
 
     // Users signing up to attend an event
-    public boolean joinEvent(User user, Event event);
+    public boolean joinEvent(User user, Event event, final DatabaseCallback<Integer> callback);
 
     // Returns all users currently signed up for an event
-    public ArrayList<User> getUsersAtEvent(Event event);
+    public ArrayList<User> getUsersAtEvent(Event event, final DatabaseCallback<Integer> callback);
 
     // Returns all events an individual has signed up for
-    public ArrayList<Event> getJoinedEvents(User user);
+    public ArrayList<Event> getJoinedEvents(User user, final DatabaseCallback<Integer> callback);
 
 
 
     ///////////////// Activity Methods /////////////////
 
-    public boolean addActivity(Activity activity);
+    public boolean addActivity(Activity activity, final DatabaseCallback<Integer> callback);
 
-    public Activity getActivityByID(int activityID);
+    public Activity getActivityByID(int activityID, final DatabaseCallback<Integer> callback);
 
-    public ArrayList<Activity> getAllActivities(Event event);
+    public ArrayList<Activity> getAllActivities(Event event, final DatabaseCallback<Integer> callback);
 
     // Visit (Visit class includes user/event details)
-    public boolean addVisit(Visit visit);
+    public boolean addVisit(Visit visit, final DatabaseCallback<Integer> callback);
 
-    public Visit getVisitByID(int userID, int activityID);
+    public Visit getVisitByID(int userID, int activityID, final DatabaseCallback<Integer> callback);
 
     // Gives the total visits for a specific user
-    public int visitCountForUser(int userID);
+    public int visitCountForUser(int userID, final DatabaseCallback<Integer> callback);
 
     // Gives the total visits at an event for a specific user
-    public int visitCountForUserAtEvent(int userID, int eventID);
+    public int visitCountForUserAtEvent(int userID, int eventID, final DatabaseCallback<Integer> callback);
 
     // Gives the total visits for a specific activity
-    public int visitCountAtActivity(int activityID);
+    public int visitCountAtActivity(int activityID, final DatabaseCallback<Integer> callback);
 
 
 
     ///////////////// User Methods /////////////////
 
-    public boolean addUser(User user);
+    public boolean addUser(User user, final DatabaseCallback<Integer> callback);
 
-    public User getUserByID(int userID);
+    public User getUserByID(int userID, final DatabaseCallback<Integer> callback);
 
-    public ArrayList<User> getAllUsers();
+    public ArrayList<User> getAllUsers(final DatabaseCallback<Integer> callback);
 
-    public boolean verifyPassword(String password, User user);
+    public boolean verifyPassword(String password, User user, final DatabaseCallback<Integer> callback);
 
 
 
     ///////////////// Location Methods /////////////////
 
     // Gives all the activities (in a given event) that overlap a point on the map
-    public ArrayList<Activity> activitiesAtLocation(GPSLocation location, Event event);
+    public ArrayList<Activity> activitiesAtLocation(GPSLocation location, Event event, final DatabaseCallback<Integer> callback);
 
 }
--- app/src/main/java/com/example/myapplication/database/DatabaseManager.java
+++ app/src/main/java/com/example/myapplication/database/DatabaseManager.java
@@ -1,29 +1,17 @@
 package com.example.myapplication.database;
 
 import android.content.Context;
-import android.os.AsyncTask;
-import android.os.Handler;
-import android.os.Looper;
-import android.util.Log;
 
 import com.example.myapplication.component.Activity;
 import com.example.myapplication.component.Event;
-import com.example.myapplication.component.GeneralUser;
-import com.example.myapplication.component.OrganisationUser;
 import com.example.myapplication.component.User;
 import com.example.myapplication.component.Visit;
 import com.example.myapplication.location.GPSLocation;
 
-import java.net.HttpURLConnection;
 import java.util.ArrayList;
-
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -32,8 +20,7 @@ public class DatabaseManager implements DatabaseInterface {
 
     private RequestQueue requestQueue;
     private Context context;
-
-    String baseUrl = "http://comp90018.us.to:8080";
+    private static String baseUrl = "http://comp90018.us.to:8080";
 
     public DatabaseManager(Context context) {
         // Initialize the Volley RequestQueue
@@ -73,97 +60,97 @@ public class DatabaseManager implements DatabaseInterface {
     }
 
     @Override
-    public boolean addEvent(Event event) {
+    public boolean addEvent(Event event, DatabaseCallback<Integer> callback) {
         return false;
     }
 
     @Override
-    public Event getEventByID(int eventID) {
+    public Event getEventByID(int eventID, DatabaseCallback<Integer> callback) {
         return null;
     }
 
     @Override
-    public ArrayList<Event> getAllEvents() {
+    public ArrayList<Event> getAllEvents(DatabaseCallback<Integer> callback) {
         return null;
     }
 
     @Override
-    public boolean joinEvent(User user, Event event) {
+    public boolean joinEvent(User user, Event event, DatabaseCallback<Integer> callback) {
         return false;
     }
 
     @Override
-    public ArrayList<User> getUsersAtEvent(Event event) {
+    public ArrayList<User> getUsersAtEvent(Event event, DatabaseCallback<Integer> callback) {
         return null;
     }
 
     @Override
-    public ArrayList<Event> getJoinedEvents(User user) {
+    public ArrayList<Event> getJoinedEvents(User user, DatabaseCallback<Integer> callback) {
         return null;
     }
 
     @Override
-    public boolean addActivity(Activity activity) {
+    public boolean addActivity(Activity activity, DatabaseCallback<Integer> callback) {
         return false;
     }
 
     @Override
-    public Activity getActivityByID(int activityID) {
+    public Activity getActivityByID(int activityID, DatabaseCallback<Integer> callback) {
         return null;
     }
 
     @Override
-    public ArrayList<Activity> getAllActivities(Event event) {
+    public ArrayList<Activity> getAllActivities(Event event, DatabaseCallback<Integer> callback) {
         return null;
     }
 
     @Override
-    public boolean addVisit(Visit visit) {
+    public boolean addVisit(Visit visit, DatabaseCallback<Integer> callback) {
         return false;
     }
 
     @Override
-    public Visit getVisitByID(int userID, int activityID) {
+    public Visit getVisitByID(int userID, int activityID, DatabaseCallback<Integer> callback) {
         return null;
     }
 
     @Override
-    public int visitCountForUser(int userID) {
+    public int visitCountForUser(int userID, DatabaseCallback<Integer> callback) {
         return 0;
     }
 
     @Override
-    public int visitCountAtActivity(int activityID) {
+    public int visitCountForUserAtEvent(int userID, int eventID, DatabaseCallback<Integer> callback) {
         return 0;
     }
 
     @Override
-    public int visitCountForUserAtEvent(int userID, int eventID) {
+    public int visitCountAtActivity(int activityID, DatabaseCallback<Integer> callback) {
         return 0;
     }
 
     @Override
-    public boolean addUser(User user) {
+    public boolean addUser(User user, DatabaseCallback<Integer> callback) {
         return false;
     }
 
     @Override
-    public User getUserByID(int userID) {
+    public User getUserByID(int userID, DatabaseCallback<Integer> callback) {
         return null;
     }
 
     @Override
-    public ArrayList<User> getAllUsers() {
+    public ArrayList<User> getAllUsers(DatabaseCallback<Integer> callback) {
         return null;
     }
 
     @Override
-    public boolean verifyPassword(String password, User user) {
+    public boolean verifyPassword(String password, User user, DatabaseCallback<Integer> callback) {
         return false;
     }
 
     @Override
-    public ArrayList<Activity> activitiesAtLocation(GPSLocation location, Event event) {
+    public ArrayList<Activity> activitiesAtLocation(GPSLocation location, Event event, DatabaseCallback<Integer> callback) {
         return null;
     }
 }
