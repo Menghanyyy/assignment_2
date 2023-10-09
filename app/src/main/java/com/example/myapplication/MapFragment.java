4c9f1802df91d043e930f8d07ef9e8a2b21c4609
PsyHuskie <leeshifoo@gmail.com>
2023-10-09 23:37:14 +1030

    test

--- app/src/main/java/com/example/myapplication/MapFragment.java
+++ app/src/main/java/com/example/myapplication/MapFragment.java
@@ -47,6 +47,7 @@ public class MapFragment extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
