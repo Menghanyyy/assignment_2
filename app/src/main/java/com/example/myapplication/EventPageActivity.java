118e8e70ec305b749cad3933c70b4f39e4cb90d8
PsyHuskie <leeshifoo@gmail.com>
2023-10-26 21:31:31 +1030

    change on the event page and map view

--- app/src/main/java/com/example/myapplication/EventPageActivity.java
+++ app/src/main/java/com/example/myapplication/EventPageActivity.java
@@ -24,6 +24,7 @@ public class EventPageActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
 
     DatabaseManager databaseManager;
+    TextView tv_gotomap;
 
 
     @SuppressLint("MissingInflatedId")
@@ -33,7 +34,15 @@ public class EventPageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_card_info);
         Intent intent = getIntent();
         String eventId = intent.getStringExtra("eventId");
+        tv_gotomap = findViewById(R.id.tv_gotomap);
+        tv_gotomap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent  mapIntent = new Intent(EventPageActivity.this,MapActivity.class);
+                startActivity(mapIntent);
 
+            }
+        });
         databaseManager = new DatabaseManager(this);
 
         databaseManager.getEventByID(Integer.parseInt(eventId), new DatabaseCallback<Event>() {
