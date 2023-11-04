28f53887b308dc5ff41e126078025db273b8e0b1
adrian001121 <adrianyikiat@student.unimelb.edu.au>
2023-11-04 22:30:22 +1100

    Adding Calander to Activity

--- app/src/main/java/com/example/myapplication/AddRemoveActivity.java
+++ app/src/main/java/com/example/myapplication/AddRemoveActivity.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 
 import android.Manifest;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -13,8 +15,10 @@ import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.activity.result.ActivityResult;
@@ -34,17 +38,25 @@ import org.w3c.dom.Text;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class AddRemoveActivity extends AppCompatActivity {
 
     private static final int REQUEST_PERMISSIONS = 1001;
+
+    private Calendar dateTimeCalendar;
 
     ImageView activity_image;
     TextView activity_name;
     TextView activity_description;
     TextView activity_organisation;
     TextView activity_address;
+    TextView activity_start_time;
+    TextView activity_end_time;
 
     TextView add_activity_button;
 
@@ -56,11 +68,14 @@ public class AddRemoveActivity extends AppCompatActivity {
     LatLng activityCenter;
     ArrayList<LatLng> activityRange;
 
+    int timeStatusSelect = 0;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_activity);
 
+        dateTimeCalendar = Calendar.getInstance();
 
         activity_image = findViewById(R.id.activity_image);
         activity_image.setDrawingCacheEnabled(true);
@@ -70,6 +85,8 @@ public class AddRemoveActivity extends AppCompatActivity {
         activity_description = findViewById(R.id.activity_description);
         activity_organisation = findViewById(R.id.activity_organisation);
         activity_address = findViewById(R.id.activity_address);
+        activity_start_time = findViewById(R.id.activity_start_time);
+        activity_end_time = findViewById(R.id.activity_end_time);
 
         add_activity_button = findViewById(R.id.activity_register_map_button);
         confirm_activity_button = findViewById(R.id.activity_button);
@@ -81,6 +98,17 @@ public class AddRemoveActivity extends AppCompatActivity {
         confirm_activity_button.setVisibility(View.GONE);
         registerMapCenterStatus.setVisibility(View.GONE);
         registerMapRangeStatus.setVisibility(View.GONE);
+
+        activity_start_time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                new DatePickerDialog(AddRemoveActivity.this, dateSetListener,
+                        dateTimeCalendar.get(Calendar.YEAR),
+                        dateTimeCalendar.get(Calendar.MONTH),
+                        dateTimeCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
 
         activity_image.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -243,6 +271,42 @@ public class AddRemoveActivity extends AppCompatActivity {
             Toast.makeText(this, "Permission denied!", Toast.LENGTH_SHORT).show();
         }
     }
+
+    private DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+            // Set the Calendar new date chosen by user
+            dateTimeCalendar.set(Calendar.YEAR, year);
+            dateTimeCalendar.set(Calendar.MONTH, month);
+            dateTimeCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+            // Now that we have the date, show the time picker
+            new TimePickerDialog(AddRemoveActivity.this, timeSetListener,
+                    dateTimeCalendar.get(Calendar.HOUR_OF_DAY),
+                    dateTimeCalendar.get(Calendar.MINUTE), true).show();
+        }
+    };
+
+    private TimePickerDialog.OnTimeSetListener timeSetListener = new TimePickerDialog.OnTimeSetListener() {
+        @Override
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            // Set the Calendar new time chosen by user
+            dateTimeCalendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+            dateTimeCalendar.set(Calendar.MINUTE, minute);
+
+            // Format the dateTimeCalendar to your liking and display it
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault());
+            activity_start_time.setText(dateFormat.format(dateTimeCalendar.getTime()));
+
+            // Use the dateTimeCalendar.getTime() as a Date object
+            Date date = dateTimeCalendar.getTime();
+
+            // If you need to convert this Date object to the old java.sql.Timestamp
+            java.sql.Timestamp timeStampDate = new java.sql.Timestamp(date.getTime());
+
+            // TODO: Do something with the date or timestamp
+        }
+    };
 
 
 }
